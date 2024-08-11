2cf0c13809786dcd8a845ecd9d9ed93375e5b25b
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-08-11 12:45:29 -0700

    feat: vial works!

---
+++ keyboards/zsa/moonlander/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+#define AUDIO_CLICKY
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+
+// Unique keyboard id
+#define VIAL_KEYBOARD_UID {0x20, 0xF2, 0xD5, 0xEA, 0xD8, 0x72, 0x04, 0xA6}
+
+// Unlock with keys "2" and "4"
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 4 }
