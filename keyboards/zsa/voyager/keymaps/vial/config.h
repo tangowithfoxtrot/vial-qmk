ca48314def6e8a961f42d23449511363219f64c6
tangowithfoxtrot <5676771+tangowithfoxtrot@users.noreply.github.com>
2024-09-18 19:29:49 -0700

    vial works!

---
+++ keyboards/zsa/voyager/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+// Unique keyboard id
+#define VIAL_KEYBOARD_UID {0x3D, 0x3A, 0x3B, 0x3C, 0x3D, 0x3E, 0x3F, 0x40}
+
+// Unlock with keys "1" and "3"
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 2, 4 }
